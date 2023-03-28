6497e269551171521df6460f94b8f7c5eb344317
Jonas Miederer <hello@jonas-miederer.de>
2023-03-28 16:03:36 +0200

    added toolbar

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { Grid, GridItem, PageSection, Title } from '@patternfly/react-core';
+import { Button, Grid, GridItem, PageSection, Title, Toolbar, ToolbarContent, ToolbarItem } from '@patternfly/react-core';
 import { ProductSearch } from './components/ProductSearch';
 import { Cart } from './components/Cart';
 import { PosApi, Configuration } from '@app/api';
-import { CustomerPane } from './components/CustomerInfo';
+import { CustomerPane } from './components/CustomerPane';
+import { TimesIcon, TrashIcon } from '@patternfly/react-icons';
 
 const Dashboard: React.FunctionComponent = () => {
   const [product, setProduct] = React.useState();
@@ -17,14 +18,38 @@ const Dashboard: React.FunctionComponent = () => {
     setCartItems((cartItems) => [...cartItems, { product: product, type: type }])
   }
 
+  const reset = () => {
+    setCustomer(null);
+    setCartItems([]);
+  }
+
+  const toolbarItems = (
+    <React.Fragment>
+      <ToolbarItem className='pf-u-text-align-right'>
+        <Button variant="plain" aria-label="sync" onClick={() => { reset() }}>
+          <TimesIcon /> Clear
+        </Button>
+      </ToolbarItem>
+    </React.Fragment>
+  );
+
   return <PageSection>
+    <Toolbar
+      className="pf-m-toggle-group-container"
+      collapseListedFiltersBreakpoint="xl"
+    >
+      <ToolbarContent>{toolbarItems}</ToolbarContent>
+    </Toolbar>
+
     <Grid hasGutter>
-      <GridItem span={8}>
+      <GridItem span={7}>
         <ProductSearch apiClient={apiClient} product={product} setProduct={setProduct} addCartItem={addCartItem}></ProductSearch>
       </GridItem>
-      <GridItem span={4}>
+      <GridItem span={2}>
         <CustomerPane apiClient={apiClient} customer={customer} setCustomer={setCustomer}></CustomerPane>
-        <Cart apiClient={apiClient} cartItems={cartItems} setCartItems={setCartItems}></Cart>
+      </GridItem>
+      <GridItem span={3}>
+        <Cart apiClient={apiClient} cartItems={cartItems} setCartItems={setCartItems} customerDiscount={customer ? customer.discount : 0}></Cart>
       </GridItem>
     </Grid>
   </PageSection>
